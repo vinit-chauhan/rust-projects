1ed22b6e3b25d8cf018cdfc4a54609a868a78509
vinit-chauhan <chauhanvinit23@gmail.com>
2024-06-02 15:40:45 -0400

    Convert byte array to string

--- udp-socket/client/src/main.rs
+++ udp-socket/client/src/main.rs
@@ -1,5 +1,6 @@
 use std::io;
 use std::net::UdpSocket;
+use std::str::from_utf8;
 
 fn main() -> io::Result<()> {
     let socket = UdpSocket::bind("127.0.0.1:8001")?;
@@ -9,9 +10,13 @@ fn main() -> io::Result<()> {
     socket.send(message)?;
 
     let mut buffer: [u8; 1024] = [0u8; 1024];
-    let (_amt, src) = socket.recv_from(&mut buffer)?;
+    let (len, src) = socket.recv_from(&mut buffer)?;
 
-    println!("Received [{}]> {:?}", src, &buffer.to_ascii_lowercase());
+    println!(
+        "Received [{}]> {:?}",
+        src,
+        from_utf8(&buffer[..len]).unwrap()
+    );
 
     Ok(())
 }
--- udp-socket/server/src/main.rs
+++ udp-socket/server/src/main.rs
@@ -1,4 +1,5 @@
 use std::net::UdpSocket;
+use std::str::from_utf8;
 use std::{io, process::exit};
 
 use logger::{LogType, Logger};
@@ -21,14 +22,17 @@ fn main() {
     let mut buf: [u8; 1024] = [0u8; 1024];
     let (len, src) = socket.recv_from(&mut buf).expect("Unable to read message");
 
-    println!("received [{}]> {:?}", &src.to_string(), &buf[..len]);
+    println!(
+        "received [{}]> {:?}",
+        &src.to_string(),
+        from_utf8(&buf[..len]).unwrap()
+    );
 
     logger.write_info("Sending message to client.");
 
-    let ret_buf = &mut buf[..len];
-
-    println!("{:?}", &ret_buf);
-    let len = socket.send_to(&ret_buf, &src).expect("Unable to send data");
+    let len = socket
+        .send_to(&buf[..len], &src)
+        .expect("Unable to send data");
 
     logger.write(
         format!("{len} bytes sent successfully").as_str(),
